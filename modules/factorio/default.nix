bfa5d5759613978fe1cd5b704dc6e1c04a7a8afa
Jonathan Strickland <djanatyn@gmail.com>
2020-06-12 23:09:07 -0400

    upgrade factorio (0.18.31)

--- modules/factorio/default.nix
+++ modules/factorio/default.nix
@@ -1,6 +1,17 @@
 { config, pkgs, lib, ... }:
 with lib;
-let cfg = config.flowercluster.services.factorio;
+let
+  cfg = config.flowercluster.services.factorio;
+  factorio-latest = pkgs.factorio-headless-experimental.overrideAttrs
+    (old: rec {
+      version = "0.18.31";
+
+      src = pkgs.fetchurl {
+        name = "factorio_headless_linux64-0.18.31.tar.xz";
+        url = "https://www.factorio.com/get-download/0.18.31/headless/linux64";
+        sha256 = "17jslya538k910ppgfshzhxzbaxlf92ldbbcy1bhjyarmhwyk0i8";
+      };
+    });
 in {
   options = {
     flowercluster.services.factorio.enable = mkEnableOption "factorio";
@@ -8,7 +19,7 @@ in {
 
   config = mkIf cfg.enable {
     services.factorio.enable = true;
-    services.factorio.package = pkgs.factorio-headless-experimental;
+    services.factorio.package = factorio-latest;
 
     services.factorio.password =
       lib.fileContents /var/src/secrets/factorio/password;
