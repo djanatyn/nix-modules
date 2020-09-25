79cdee1273adc5f1c23a9bb0dc4f1ea7a224c7e6
Jonathan Strickland <djanatyn@gmail.com>
2020-09-25 06:40:59 -0400

    populate ~/.Xresources, use nord

--- modules/djanatyn/default.nix
+++ modules/djanatyn/default.nix
@@ -57,6 +57,8 @@ in {
       } // optionalAttrs (cfg.email.mbsync.enable) {
         ".mbsyncrc".text = fileContents ./files/mbsyncrc;
         ".davmail.properties".text = fileContents ./files/davmail.properties;
+      } // optionalAttrs (pkgs.stdenv.system == "x86_64-linux") {
+        ".Xresources".text = fileContents ./files/Xresources;
       };
 
       services.gpg-agent = {
