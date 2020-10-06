9e0b901b3e56c824acb2d19e383ebb6a7116419c
Jonathan Strickland <djanatyn@gmail.com>
2020-10-06 12:07:23 -0400

    add xresources with nord

--- home-manager/djanatyn/default.nix
+++ home-manager/djanatyn/default.nix
@@ -41,6 +41,22 @@ in {
       ".Xresources".text = fileContents ./files/.Xresources;
     };
 
+    xresources = {
+      properties = {
+        "urxvt.font" = "xft:Terminess Powerline";
+        "urxvt.scrollbar" = false;
+        "urxvt*depth" = 32;
+        "urxvt*background" = "rgba:0000/0000/0200/c800";
+      };
+
+      extraConfig = builtins.readFile (pkgs.fetchFromGitHub {
+        owner = "arcticicestudio";
+        repo = "nord-xresources";
+        rev = "ad8d70435ee0abd49acc7562f6973462c74ee67d";
+        sha256 = "1labc69iis5nx7cz88lkp0jsm5aq3ywih15g1fhc4814hv0x0ckf";
+      } + "/src/nord");
+    };
+
     programs = {
       git = {
         enable = true;
