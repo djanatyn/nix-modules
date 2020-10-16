c20534abb4d02faa2127c4dea0e63483e44e3c3e
Jonathan Strickland <djanatyn@gmail.com>
2020-10-16 12:00:09 -0400

    generate organize-tool configuration

---
+++ home-manager/djanatyn/organize.nix
@@ -0,0 +1,35 @@
+let
+  rule = source: m: {
+    folder = source;
+    subfolders = false;
+    filters = { extension = m.extensions; };
+    actions = { move = m.dest; };
+  };
+
+  layout = [
+    {
+      dest = "archives";
+      extensions = [ "zip" "gz" "dmg" "pkg" "7z" ];
+    }
+    {
+      dest = "media";
+      extensions = [ "mp4" "mov" "m4v" "avi" "wav" ];
+    }
+    {
+      dest = "image";
+      extensions = [ "gif" "jpg" "jpeg" "png" "svg" "HEIC" "webm" ];
+    }
+    {
+      dest = "binary";
+      extensions = [ "app" "deb" "AppImage" "msi" "exe" ];
+    }
+    {
+      dest = "font";
+      extensions = [ "ttf" "dfont" ];
+    }
+    {
+      dest = "document";
+      extensions = [ "txt" "csv" "json" "pdf" "epub" ];
+    }
+  ];
+in { rules = map (rule "~/Downloads") layout; }
