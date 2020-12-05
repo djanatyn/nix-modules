5e7897d71b87c022539ebabb95d3c140a6e66b56
Jonathan Strickland <djanatyn@gmail.com>
2020-12-05 14:28:48 -0500

    doom: add ivy module options
    +childframe is very cool!

--- home-manager/djanatyn/files/.doom.d/init.el
+++ home-manager/djanatyn/files/.doom.d/init.el
@@ -1,7 +1,7 @@
 (doom!
  :checkers (syntax-checker +childframe)
 
- :completion (company +auto) ivy
+ :completion (company +auto) (ivy +childframe +icons +fuzzy)
 
  :ui doom doom-dashboard doom-quit modeline ophints fill-column hl-todo
  nav-flash (popup +all +defaults) ligatures vc-gutter vi-tilde-fringe
